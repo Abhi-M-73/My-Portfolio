53190fdf0cd09087b5d6377665d36379b71e8575
Abhishek <abhishekmaddheshiya632@gmail.com>
2024-09-12 15:22:52 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='h-screen w-full bg-black flex items-center justify-center text-white'>
+      <h1 className='text-5xl'>hello word</h1>
+    </div>
+  )
+}
+
+export default App
